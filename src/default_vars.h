060e7c96aee988c30044c53d3e1b212ecc894721
Michael Sheppard <mike@zeus>
2012-07-02 16:10:46 -0400

    code cleanup and removed un-used images

--- src/default_vars.h
+++ src/default_vars.h
@@ -1,5 +1,26 @@
 #ifndef _default_vars_h_
 #define _default_vars_h_
+// gbzadmin
+// GTKmm bzadmin
+// Copyright (c) 2005 - 2012 Michael Sheppard
+//  
+// Code based on BZFlag-2.0.x
+// Portions Copyright (c) 1993 - 2005 Tim Riker
+// 
+//  =====GPL=============================================================
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; version 2 dated June, 1991.
+// 
+//  This program is distributed in the hope that it will be useful, 
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program;  if not, write to the Free Software
+//  Foundation, Inc., 675 Mass Ave., Cambridge, MA 02139, USA.
+//  =====================================================================
 
 // 2.4.0 default variable values
 static const char* defaults[][256] = {
